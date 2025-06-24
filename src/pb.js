8940260c55a40f6e92bbbe707689a90ae32dc041
javohirwebdeveloper <jumaboyevjavohir07@gmail.com>
2025-06-24 18:44:40 +0500

    u

--- src/pb.js
+++ src/pb.js
@@ -2,7 +2,6 @@ import PocketBase from 'pocketbase';
 
 const pb = new PocketBase('http://127.0.0.1:8090');
 
-// Avvalgi sessiyani tiklash
 const stored = localStorage.getItem('pb_auth');
 if (stored) {
   try {
@@ -13,7 +12,6 @@ if (stored) {
   }
 }
 
-// Token o‘zgarganda localStorage ni yangilash
 pb.authStore.onChange((token, model) => {
   if (token && model) {
     localStorage.setItem('pb_auth', JSON.stringify({ token, model }));
