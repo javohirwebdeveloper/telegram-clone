a6042738630cdc453adf0f08098b62b795f45906
javohirwebdeveloper <jumaboyevjavohir07@gmail.com>
2025-06-25 16:15:57 +0500

    i

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,3 @@
-
 // src/components/Sidebar.jsx
 import { useEffect, useState } from 'react';
 import pb from '../pb';
@@ -16,17 +15,35 @@ export default function Sidebar({ onSelect, selectedUser }) {
   }, []);
 
   return (
-    <div className="w-1/3 border-r border-[#202c33] h-screen overflow-auto bg-[#212121] text-white">
+    <div className="w-2/7 border-r px-2 border-[#202c33] h-screen overflow-auto bg-[#212121] text-white">
       <h2 className="p-4 font-bold text-xl border-b border-[#202c33]">Users</h2>
-      {users.map(user => (
-        <div
-          key={user.id}
-          className={`p-4 border-b border-[#202c33] cursor-pointer hover:bg-[#2a3942] ${selectedUser?.id === user.id ? 'bg-[#202c33]' : ''}`}
-          onClick={() => onSelect(user)}
-        >
-          {user.username || user.email || user.id}
-        </div>
-      ))}
+      {users.map(user => {
+        const avatarUrl = user.avatar
+          ? pb.files.getUrl(user, user.avatar)
+          : null;
+        const firstLetter = (user.username || user.email || user.id)?.charAt(0)?.toUpperCase();
+
+        return (
+          <div
+            key={user.id}
+            className={`flex items-center gap-3 p-[10px] border-0 rounded-2xl cursor-pointer hover:bg-[#2C2C2C] ${selectedUser?.id === user.id ? 'bg-[#766ac8]' : ''}`}
+            onClick={() => onSelect(user)}
+          >
+            {avatarUrl ? (
+              <img
+                src={avatarUrl}
+                alt="avatar"
+                className="w-15 h-15 rounded-full object-cover"
+              />
+            ) : (
+              <div className="w-15 h-15 text-2xl rounded-full bg-[#3a4a54] flex items-center justify-center text-white font-semibold">
+                {firstLetter}
+              </div>
+            )}
+            <span className="truncate text-[16px]">{user.username || user.email || user.id}</span>
+          </div>
+        );
+      })}
     </div>
   );
 }
